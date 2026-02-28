6e4cdad8412bb37f36b419f243b29a877f7e42f8
maya <abbadmaya1@gmail.com>
2026-02-28 21:48:58 +0100

    build script modif

--- build-images.sh
+++ build-images.sh
@@ -3,7 +3,4 @@ echo "Construction des images Docker..."
 docker build -t cineapi:latest ./cineapi
 docker build -t cinefront:latest ./cinefront
 echo "Images construites avec succès !"
-# On commente ou supprime le push
-# docker push chatodo/cineapi:latest
-# docker push chatodo/cinefront:latest
 echo "Terminé !"
