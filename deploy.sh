981e8aa1b98f2615d64cd7d0be58c7a4dbab1855
maya <abbadmaya1@gmail.com>
2026-03-01 00:22:18 +0100

    MAJ des images vers Docker Hub (maymay1008/..) avec policy : IfNotPresent

--- deploy.sh
+++ deploy.sh
@@ -6,3 +6,5 @@ sleep 10
 kubectl apply -f k8s/cineapi.yml
 kubectl apply -f k8s/cinefront.yml
 kubectl apply -f k8s/istio-gateway.yml
+kubectl apply -f k8s/mtls/strict-mtls.yml
+kubectl apply -f k8s/mtls/destination-rule-mtls.yml
